75fddcb411e3e4aa07482d4c4d22e427430fd024
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-08-12 23:47:34 +0900

    Show duration in Admin

--- pomodoro/admin.py
+++ pomodoro/admin.py
@@ -4,7 +4,7 @@ from pomodoro.models import Pomodoro
 
 
 class PomodoroAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'owner', 'created')
+    list_display = ('title', 'category', 'duration', 'owner', 'created')
     list_filter = ('owner', 'category',)
 
 admin.site.register(Pomodoro, PomodoroAdmin)
