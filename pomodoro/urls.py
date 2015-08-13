d8a90d99fb4a62f35418a5cb9828e0dbfa344dcb
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-08-13 10:25:40 +0900

    Bar Chart and Line Chart for Pomodoros

--- pomodoro/urls.py
+++ pomodoro/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 import pomodoro.views
 
 urlpatterns = patterns(
     '',
+    url(r'^bar', TemplateView.as_view(template_name="barchart.html")),
     url(r'^calendar$', pomodoro.views.PomodoroCalendarView.as_view()),
     url(r'^chart$', pomodoro.views.ChartView.as_view()),
+    url(r'^line', TemplateView.as_view(template_name="lineplot.html")),
 )
