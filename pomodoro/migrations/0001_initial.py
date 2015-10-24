abd62b12f2c7513cb782c808381224f6012d6b63
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-10-24 10:30:58 +0900

    Initial Migration

---
+++ pomodoro/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Pomodoro',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('created', models.DateTimeField(default=True)),
+                ('duration', models.IntegerField()),
+                ('title', models.TextField()),
+                ('category', models.TextField(blank=True)),
+                ('owner', models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='pomodoros')),
+            ],
+            options={
+                'ordering': ('-created',),
+            },
+        ),
+    ]
