d41bfe6d920fc98bc19d68c27db2c9dcf005477c
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-11-30 19:56:23 +0900

    Add links for the different pomodoro views.

--- pomodoro/urls.py
+++ pomodoro/urls.py
@@ -6,10 +6,10 @@ import pomodoro.views
 
 urlpatterns = patterns(
     '',
-    url(r'^bar', login_required(TemplateView.as_view(template_name="charts/bar.html"))),
-    url(r'^pie', login_required(TemplateView.as_view(template_name="charts/pie.html"))),
-    url(r'^today', login_required(TemplateView.as_view(template_name="charts/today.html"))),
-    url(r'^yesterday', login_required(TemplateView.as_view(template_name="charts/yesterday.html"))),
+    url(r'^bar', login_required(TemplateView.as_view(template_name="charts/bar.html")), name='bar'),
+    url(r'^pie', login_required(TemplateView.as_view(template_name="charts/pie.html")), name='pie'),
+    url(r'^today', login_required(TemplateView.as_view(template_name="charts/today.html")), name='today'),
+    url(r'^yesterday', login_required(TemplateView.as_view(template_name="charts/yesterday.html")), name='yesterday'),
     url(r'^calendar$', pomodoro.views.PomodoroCalendarView.as_view()),
-    url(r'^line', login_required(TemplateView.as_view(template_name="charts/annotation.html"))),
+    url(r'^line', login_required(TemplateView.as_view(template_name="charts/annotation.html")), name='line'),
 )
