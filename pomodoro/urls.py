b19e15d01bd9e2a746dcdf70845a22884b93285c
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-12-22 11:13:46 +0900

    Update urls for Django 1.9

--- pomodoro/urls.py
+++ pomodoro/urls.py
@@ -1,15 +1,14 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
 import pomodoro.views
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^bar', login_required(TemplateView.as_view(template_name="charts/bar.html")), name='bar'),
     url(r'^pie', login_required(TemplateView.as_view(template_name="charts/pie.html")), name='pie'),
     url(r'^today', login_required(TemplateView.as_view(template_name="charts/today.html")), name='today'),
     url(r'^yesterday', login_required(TemplateView.as_view(template_name="charts/yesterday.html")), name='yesterday'),
     url(r'^calendar$', pomodoro.views.PomodoroCalendarView.as_view()),
     url(r'^line', login_required(TemplateView.as_view(template_name="charts/annotation.html")), name='line'),
-)
+]
