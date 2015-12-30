f8756fff5594762918af03c124b6a49ac543c99f
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-12-30 21:27:28 +0900

    Minor date query fixes
    * Use timezone.now() and timezone.localtime() so that our query makes
    more sense for our user
    * Default to two weeks of pomodoros to help with other calculations

--- pomodoro/views.py
+++ pomodoro/views.py
@@ -2,14 +2,16 @@ import datetime
 import logging
 
 import pytz
+from icalendar import Calendar, Event
+
+from pomodoro import __homepage__, __version__
+from pomodoro.models import Pomodoro
+
 from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.utils import timezone
 from django.views.generic.base import View
-from icalendar import Calendar, Event
-from pomodoro import __version__, __homepage__
-
-from pomodoro.models import Pomodoro
 
 try:
     from rest_framework.authtoken.models import Token
@@ -20,7 +22,7 @@ logger = logging.getLogger(__name__)
 
 
 class PomodoroCalendarView(View):
-    limit = 7
+    limit = 14
 
     def get(self, request, *args, **kwargs):
         if not request.user.is_authenticated():
@@ -42,7 +44,8 @@ class PomodoroCalendarView(View):
         cal.add('X-GENERATOR', __homepage__)
         cal.add('X-GENERATOR-VERSION', __version__)
 
-        today = datetime.datetime.utcnow()
+        # Query today based on the local timezone then strip the timestamps to set it to 00:00
+        today = timezone.localtime(timezone.now()).replace(hour=0, minute=0, second=0, microsecond=0)
         query = today - datetime.timedelta(days=self.limit)
         pomodoros = Pomodoro.objects.order_by('-created').filter(
             owner=request.user,
