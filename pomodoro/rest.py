bc5b8adc2820cb33969be81436c8ab9c98007d1f
Paul Traylor <kungfudiscomonkey@gmail.com>
2017-08-12 21:05:36 +0900

    Remove some old endpoints to simplify api

--- pomodoro/rest.py
+++ pomodoro/rest.py
@@ -5,19 +5,16 @@ import logging
 import time
 
 import pytz
-from rest_framework import status, viewsets
-from rest_framework.authentication import (BasicAuthentication,
-                                           SessionAuthentication,
+from rest_framework import viewsets
+from rest_framework.authentication import (SessionAuthentication,
                                            TokenAuthentication)
-from rest_framework.decorators import list_route
-from rest_framework.response import Response
+from rest_framework.decorators import list_route, detail_route
 
 from pomodoro.models import Favorite, Pomodoro
 from pomodoro.permissions import IsOwner
 from pomodoro.renderers import CalendarRenderer
 from pomodoro.serializers import FavoriteSerializer, PomodoroSerializer
-
-from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from django.utils import timezone
 from django.utils.timezone import make_aware
@@ -41,7 +38,7 @@ class FavoriteViewSet(viewsets.ModelViewSet):
     queryset = Favorite.objects.all()
     serializer_class = FavoriteSerializer
     permission_classes = (IsOwner,)
-    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -52,26 +49,11 @@ class FavoriteViewSet(viewsets.ModelViewSet):
         """
         return Favorite.objects.filter(owner=self.request.user)
 
-class PomodoroViewSet(viewsets.ModelViewSet):
-    """
-    Basic Pomodoro API without any extra
-    """
-    queryset = Pomodoro.objects.all()
-    serializer_class = PomodoroSerializer
-    permission_classes = (IsOwner,)
-    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
-    renderer_classes = viewsets.ModelViewSet.renderer_classes + [CalendarRenderer]
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-    def get_today(self):
-        return floorts(timezone.localtime(timezone.now()))
-
-    @list_route(methods=['post'])
-    def start(self, request):
-        body = json.loads(request.body.decode("utf-8"))
-
+    @detail_route(methods=['post'])
+    def start(self, request, pk):
+        '''Quickstart a Pomodoro'''
+        favorite = self.get_object()
+        now = timezone.now().replace(microsecond=0)
         pomodoro = Pomodoro.objects\
             .filter(owner=request.user).latest('start')
         now = timezone.now().replace(microsecond=0)
@@ -83,37 +65,35 @@ class PomodoroViewSet(viewsets.ModelViewSet):
             }, status=409)
 
         pomodoro = Pomodoro()
-        pomodoro.title = body['title']
-        pomodoro.category = body.get('category', '')
+        pomodoro.title = favorite.title
+        pomodoro.category = favorite.category
         pomodoro.owner = request.user
         pomodoro.start = now
-        pomodoro.end = now + datetime.timedelta(minutes=body['duration'])
+        pomodoro.end = now + datetime.timedelta(minutes=favorite.duration)
         pomodoro.save()
 
         return JsonResponse(self.serializer_class(pomodoro).data, status=201)
 
-    @list_route(methods=['post'])
-    def stop(self, request):
-        pomodoro = Pomodoro.objects\
-            .filter(owner=request.user).latest('end')
-        now = timezone.now().replace(microsecond=0)
 
-        if pomodoro.end < now:
-            return JsonResponse({
-                'message': 'No active Pomodoro',
-                'data': self.serializer_class(pomodoro).data
-            }, status=409)
+class PomodoroViewSet(viewsets.ModelViewSet):
+    """
+    Basic Pomodoro API without any extra
+    """
+    queryset = Pomodoro.objects.all()
+    serializer_class = PomodoroSerializer
+    permission_classes = (IsOwner,)
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    renderer_classes = viewsets.ModelViewSet.renderer_classes + [CalendarRenderer]
 
-        pomodoro.end = now
-        pomodoro.save()
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-        return JsonResponse({
-            'message': 'Stopped active Pomodoro',
-            'data': self.serializer_class(pomodoro).data
-        }, status=201)
+    def get_today(self):
+        return floorts(timezone.localtime(timezone.now()))
 
     @list_route(methods=['post'])
     def query(self, request):
+        '''Grafana Query'''
         body = json.loads(request.body.decode("utf-8"))
         start = make_aware(
             datetime.datetime.strptime(body['range']['from'], DATETIME_FORMAT),
@@ -174,6 +154,7 @@ class PomodoroViewSet(viewsets.ModelViewSet):
 
     @list_route(methods=['post'])
     def search(self, request):
+        '''Grafana Search'''
         categories = list(Pomodoro.objects
             .filter(owner=self.request.user)
             .exclude(category='')
@@ -184,57 +165,4 @@ class PomodoroViewSet(viewsets.ModelViewSet):
         return JsonResponse([NOCATEGORY] + categories, safe=False)
 
     def get_queryset(self):
-        """
-        This view should return a list of all the purchases
-        for the currently authenticated user.
-        """
-        qs = Pomodoro.objects.filter(owner=self.request.user)
-        date = self.request.query_params.get('date')
-        if date:
-            if date == 'today':
-                today = self.get_today()
-                return qs.filter(start__gte=today)
-            if date == 'yesterday':
-                today = self.get_today()
-                yesterday = today - datetime.timedelta(days=1)
-                return qs.filter(start__gte=yesterday, end__lt=today)
-        else:
-            days = int(self.request.query_params.get('days', 7))
-            created_after = self.get_today() - datetime.timedelta(days=days)
-            return qs.filter(start__gte=created_after)
-        return qs
-
-    @list_route(methods=['post'])
-    def append(self, request, *args, **kwargs):
-        '''
-        Log time spent on a pomodoro
-
-        This route is intended as a quick time logger. If there is an existing match for the pomodoro,
-        time will be appended, otherwise a new pomodoro object will be created
-        '''
-        serializer = self.get_serializer(data=request.data, partial=True)
-        if serializer.is_valid():
-            logger.debug('Valid serializer %s', serializer.validated_data)
-            try:
-                # We fuzz our search by about 15 seconds to account for clock
-                # drift between computers
-                search_start = serializer.validated_data['start'] - datetime.timedelta(seconds=15)
-                logger.debug('Searching from %s', search_start)
-                pomodoro = Pomodoro.objects.filter(owner=self.request.user)\
-                    .filter(title=serializer.validated_data['title'])\
-                    .filter(end__gte=search_start)\
-                    .get()
-            except ObjectDoesNotExist:
-                logger.debug('Creating new object')
-                obj = serializer.save(owner=self.request.user)
-                return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
-            else:
-                logger.debug('Updating old object')
-                serializer = self.get_serializer(pomodoro, data=request.data, partial=True)
-                if serializer.is_valid():
-                    # Make sure we keep the original start time
-                    serializer.validated_data['start'] = pomodoro.start
-                    obj = serializer.save(owner=self.request.user)
-                    return Response(serializer.validated_data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Pomodoro.objects.filter(owner=self.request.user)
