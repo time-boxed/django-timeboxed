0323804ee2b9b42f1a0d676f3b7f8a980aba4068
Paul Traylor <kungfudiscomonkey@gmail.com>
2018-11-29 21:10:09 +0900

    Formatting and updating Pipfile.lock

--- setup.py
+++ setup.py
@@ -3,49 +3,45 @@ from setuptools import find_packages, setup
 from pomodoro import __version__, __homepage__
 
 setup(
-    name='django-pomodoro',
-    description='Render Pomodoro Calendars',
-    author='Paul Traylor',
+    name="django-pomodoro",
+    description="Render Pomodoro Calendars",
+    author="Paul Traylor",
     url=__homepage__,
     version=__version__,
     packages=find_packages(),
     install_requires=[
-        'djangorestframework',
-        'icalendar',
-        'Pillow',
-        ],
+        "Django==2.1.3",
+        "djangorestframework",
+        "icalendar",
+        "Pillow",
+        "requests==2.20.1",
+    ],
     extras_require={
-        'standalone': [
-            'celery[redis]==4.2.1',
-            'dj_database_url',
-            'Django==2.1.2',
-            'envdir',
-            'prometheus_client',
-            'psycopg2',
-            'raven',
-            'social-auth-app-django==2.1.0',
+        "standalone": [
+            "celery[redis]==4.2.1",
+            "dj_database_url",
+            "envdir",
+            "prometheus_client",
+            "raven",
+            "social-auth-app-django==2.1.0",
         ],
-        'dev': [
-            'unittest-xml-reporting',
-        ]
+        "dev": ["unittest-xml-reporting"],
     },
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Framework :: Django',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
     ],
     entry_points={
-        'powerplug.apps': ['pomodoro = pomodoro.apps.PomodoroConfig'],
-        'powerplug.urls': ['pomodoro = pomodoro.urls'],
-        'powerplug.rest': [
-            'pomodoro = pomodoro.rest:PomodoroViewSet',
-            'favorite = pomodoro.rest:FavoriteViewSet',
+        "powerplug.apps": ["pomodoro = pomodoro.apps.PomodoroConfig"],
+        "powerplug.urls": ["pomodoro = pomodoro.urls"],
+        "powerplug.rest": [
+            "pomodoro = pomodoro.rest:PomodoroViewSet",
+            "favorite = pomodoro.rest:FavoriteViewSet",
         ],
-        'console_scripts': [
-            'pomodoro = pomodoro.standalone.manage:main[standalone]',
-        ],
+        "console_scripts": ["pomodoro = pomodoro.standalone.manage:main[standalone]"],
     },
 )
