7ba237be34612bdf73dd91f30c13f52c8fb3ac6d
Paul Traylor <kungfudiscomonkey@gmail.com>
2019-01-14 21:15:37 +0900

    Add options for testing

--- pomodoro/management/commands/mqtt-debug.py
+++ pomodoro/management/commands/mqtt-debug.py
@@ -18,15 +18,21 @@ class Command(BaseCommand):
         except:
             print(msg.topic, payload)
 
+    def add_arguments(self, parser):
+        parser.add_argument("-u", "--user", default=os.environ.get("MQTT_USER"))
+        parser.add_argument("-p", "--password", default=os.environ.get("MQTT_PASS"))
+        parser.add_argument("--host", default=os.environ.get("MQTT_HOST", "localhost"))
+        parser.add_argument(
+            "-P", "--port", type=int, default=os.environ.get("MQTT_PORT", 1883)
+        )
+
     def handle(self, **options):
         client = mqtt.Client()
         client.on_connect = self.on_connect
         client.on_message = self.on_message
-        client.username_pw_set(
-            os.environ["MQTT_USER"], password=os.environ["MQTT_PASS"]
-        )
+        client.username_pw_set(options["user"], password=options["password"])
 
-        client.connect(os.environ["MQTT_HOST"], 1883, 60)
+        client.connect(options["host"], options["port"], 60)
 
         # Blocking call that processes network traffic, dispatches callbacks and
         # handles reconnecting.
