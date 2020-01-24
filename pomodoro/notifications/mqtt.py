fdbccfc456d037b3b37c06b7e6efe72c4e67caf9
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-01-24 12:45:40 +0900

    Use helper function for publish

--- pomodoro/notifications/mqtt.py
+++ pomodoro/notifications/mqtt.py
@@ -1,7 +1,7 @@
 import json
 import uuid
 
-import paho.mqtt.client as mqtt
+from paho.mqtt.publish import single
 
 from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
@@ -20,8 +20,12 @@ class MQTT:
         self.publish("pomodoro/%s/recent" % pomodoro.owner.username, data)
 
     def publish(self, topic, data, retain=True):
-        client = mqtt.Client(client_id=self.client_id[:23])
-        client.username_pw_set(settings.MQTT_USER, password=settings.MQTT_PASS)
-        client.connect(settings.MQTT_HOST, settings.MQTT_PORT, 60)
-        # client.tls_set('/etc/ssl/certs/ca-bundle.trust.crt', tls_version=2)
-        client.publish(topic, json.dumps(data).encode("utf8"), retain=retain)
+        single(
+            topic=topic,
+            data=json.dumps(data).encode("utf8"),
+            retain=retain,
+            client_id=self.client_id[:23],
+            hostname=settings.MQTT_HOST,
+            port=settings.MQTT_PORT,
+            auth={"username": settings.MQTT_USER, "password": settings.MQTT_PASS},
+        )
