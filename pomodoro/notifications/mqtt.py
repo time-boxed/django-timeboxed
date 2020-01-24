1b30611117d8768b897fc0a7dc0f7a7909b40da7
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-01-24 12:13:19 +0900

    Fix for unique ID

--- pomodoro/notifications/mqtt.py
+++ pomodoro/notifications/mqtt.py
@@ -1,4 +1,5 @@
 import json
+import uuid
 
 import paho.mqtt.client as mqtt
 
@@ -10,17 +11,16 @@ from pomodoro import serializers
 
 class MQTT:
     def __init__(self, *args):
-        pass
+        self.domain = get_current_site(None).domain
+        self.uuid = uuid.uuid4()
 
     def send(self, pomodoro):
         data = serializers.PomodoroSerializer(pomodoro).data
-        data["html_url"] = (
-            "https://" + get_current_site(None).domain + pomodoro.get_absolute_url()
-        )
+        data["html_url"] = "https://" + self.domain + pomodoro.get_absolute_url()
         self.publish("pomodoro/%s/recent" % pomodoro.owner.username, data)
 
     def publish(self, topic, data, retain=True):
-        client = mqtt.Client()
+        client = mqtt.Client(client_id=self.domain + "-" + self.uuid)
         client.username_pw_set(settings.MQTT_USER, password=settings.MQTT_PASS)
         client.connect(settings.MQTT_HOST, settings.MQTT_PORT, 60)
         # client.tls_set('/etc/ssl/certs/ca-bundle.trust.crt', tls_version=2)
