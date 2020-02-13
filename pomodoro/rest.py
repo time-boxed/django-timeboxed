795da5a78955fe1570224bef716d68ec6fff9e57
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-02-13 19:14:14 +0900

    Refactor and cleanup rest api

--- pomodoro/rest.py
+++ pomodoro/rest.py
@@ -1,43 +1,17 @@
-import collections
-import datetime
-import json
-import logging
-import time
-
-import pytz
-from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.settings import api_settings
 
-from pomodoro.models import Favorite, Pomodoro
-from pomodoro.permissions import IsOwner
-from pomodoro.renderers import CalendarRenderer
-from pomodoro.serializers import FavoriteSerializer, PomodoroSerializer
+from . import models, permissions, renderers, serializers
 
 from django.http import JsonResponse
 from django.utils import timezone
-from django.utils.timezone import make_aware
-
-try:
-    from timezone.models import Timezone
-except ImportError:
-    Timezone = None
-
-DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
-NOCATEGORY = '{Uncategorized}'
-
-logger = logging.getLogger(__name__)
-
-
-def floorts(ts):
-    return ts.replace(minute=0, hour=0, second=0, microsecond=0)
 
 
 class FavoriteViewSet(viewsets.ModelViewSet):
-    queryset = Favorite.objects.all()
-    serializer_class = FavoriteSerializer
-    permission_classes = (IsOwner,)
+    queryset = models.Favorite.objects
+    serializer_class = serializers.FavoriteSerializer
+    permission_classes = (permissions.IsOwner,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -46,28 +20,35 @@ class FavoriteViewSet(viewsets.ModelViewSet):
         """
         Return Favorites owned by current user only
         """
-        return Favorite.objects.filter(owner=self.request.user)
+        return self.queryset.objects.filter(owner=self.request.user)
 
     @action(detail=True, methods=["post"])
     def start(self, request, pk):
-        '''Quickstart a Pomodoro'''
+        """Quickstart a Pomodoro"""
         now = timezone.now().replace(microsecond=0)
         try:
-            pomodoro = Pomodoro.objects\
-                .filter(owner=request.user).latest('start')
-        except Pomodoro.DoesNotExist:
+            pomodoro = models.Pomodoro.objects.filter(owner=request.user).latest(
+                "start"
+            )
+        except models.Pomodoro.DoesNotExist:
             # Handle the case for a new user that does not have any
             # pomodoros at all
             pass
         else:
             if pomodoro.end > now:
-                return JsonResponse({
-                    'message': 'Cannot replace active pomodoro',
-                    'data': PomodoroSerializer(pomodoro).data
-                }, status=409)
+                return JsonResponse(
+                    {
+                        "message": "Cannot replace active pomodoro",
+                        "data": serializers.PomodoroSerializer(pomodoro).data,
+                    },
+                    status=status.HTTP_409_CONFLICT,
+                )
 
-        favorite = self.get_object()
-        return JsonResponse(PomodoroSerializer(favorite.start(now)).data, status=201)
+        pomodoro = self.get_object().start(now)
+        return JsonResponse(
+            serializers.PomodoroSerializer(pomodoro).data,
+            status=status.HTTP_201_CREATED,
+        )
 
 
 class PomodoroViewSet(viewsets.ModelViewSet):
@@ -75,89 +56,15 @@ class PomodoroViewSet(viewsets.ModelViewSet):
     Basic Pomodoro API without any extra
     """
 
-    queryset = Pomodoro.objects.all()
-    serializer_class = PomodoroSerializer
-    permission_classes = (IsOwner,)
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES + [CalendarRenderer]
+    queryset = models.Pomodoro.objects
+    serializer_class = serializers.PomodoroSerializer
+    permission_classes = (permissions.IsOwner,)
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES + [
+        renderers.CalendarRenderer
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-    def get_today(self):
-        return floorts(timezone.localtime(timezone.now()))
-
-    @action(detail=False, methods=["post"], authentication_classes=[BasicAuthentication])
-    def query(self, request):
-        '''Grafana Query'''
-        body = json.loads(request.body.decode("utf-8"))
-        start = make_aware(
-            datetime.datetime.strptime(body['range']['from'], DATETIME_FORMAT),
-            pytz.utc)
-        end = make_aware(
-            datetime.datetime.strptime(body['range']['to'], DATETIME_FORMAT),
-            pytz.utc)
-
-        results = []
-        durations = collections.defaultdict(lambda: collections.defaultdict(int))
-
-        if Timezone:
-            tzname = Timezone.objects.filter(owner=request.user)
-            timezone.activate(pytz.timezone(tzname[0].timezone))
-            tzinfo = pytz.timezone(tzname[0].timezone)
-
-            start = start.astimezone(tzinfo)
-            end = end.astimezone(tzinfo)
-
-        _ts = floorts(start)
-        dates = []
-        for offset in range(0, (end - start).days + 1):
-            dates.append(_ts + datetime.timedelta(days=offset))
-
-        for target in body['targets']:
-            _search = '' if target['target'] == NOCATEGORY else target['target']
-            for pomodoro in Pomodoro.objects\
-                    .filter(owner=self.request.user)\
-                    .filter(category=_search)\
-                    .filter(start__gte=start)\
-                    .filter(end__lte=end):
-                # Bucket by midnight. If we have a timezone object, ensure we're in the right timezone
-                if Timezone:
-                    started = pomodoro.start.astimezone(tzinfo)
-                    completed = pomodoro.end.astimezone(tzinfo)
-                else:
-                    started = pomodoro.start
-                    completed = pomodoro.end
-
-                if started.date() == completed.date():
-                    durations[floorts(started)][target['target']] += pomodoro.duration.total_seconds()
-                else:
-                    midnight = floorts(completed)
-                    durations[floorts(started)][target['target']] += (midnight - started).total_seconds()
-                    durations[floorts(completed)][target['target']] += (completed - midnight).total_seconds()
-
-        for target in body['targets']:
-            response = {
-                'target': target['target'],
-                'datapoints': []
-            }
-
-            for ts in dates:
-                unixtimestamp = time.mktime(ts.timetuple()) * 1000
-                response['datapoints'].append([durations[ts][target['target']], unixtimestamp])
-            results.append(response)
-        return JsonResponse(results, safe=False)
-
-    @action(detail=False, methods=["post"], authentication_classes=[BasicAuthentication])
-    def search(self, request):
-        '''Grafana Search'''
-        categories = list(Pomodoro.objects
-            .filter(owner=self.request.user)
-            .exclude(category='')
-            .order_by('category')
-            .values_list('category', flat=True)
-            .distinct('category')
-        )
-        return JsonResponse([NOCATEGORY] + categories, safe=False)
-
     def get_queryset(self):
-        return Pomodoro.objects.filter(owner=self.request.user)
+        return self.queryset.filter(owner=self.request.user)
