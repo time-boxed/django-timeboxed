702a9995d163e4301804bd6cae0bf3bce2ba5db0
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-02-16 17:26:39 +0900

    Add html_link to serializers

--- pomodoro/notifications/mqtt.py
+++ pomodoro/notifications/mqtt.py
@@ -4,19 +4,16 @@ import uuid
 from paho.mqtt.publish import single
 
 from django.conf import settings
-from django.contrib.sites.shortcuts import get_current_site
 
 from pomodoro import serializers
 
 
 class MQTT:
     def __init__(self, *args):
-        self.domain = get_current_site(None).domain
         self.client_id = self.domain + "-%d" % uuid.uuid4().int
 
     def send(self, pomodoro):
         data = serializers.PomodoroSerializer(pomodoro).data
-        data["html_url"] = "https://" + self.domain + pomodoro.get_absolute_url()
         self.publish("pomodoro/%s/recent" % pomodoro.owner.username, data)
 
     def publish(self, topic, data, retain=True):
--- pomodoro/serializers.py
+++ pomodoro/serializers.py
@@ -2,11 +2,23 @@ from rest_framework import serializers
 
 from . import models
 
+from django.contrib.sites.shortcuts import get_current_site
 from django.utils import timezone
+
+
+class LinkField(serializers.Field):
+    def __init__(self, **kwargs):
+        kwargs["source"] = "get_absolute_url"
+        kwargs["read_only"] = True
+        super().__init__(**kwargs)
+
+    def to_representation(self, value):
+        return "https://" + get_current_site(None).domain + value
 
 
 class FavoriteSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    html_link = LinkField()
 
     class Meta:
         model = models.Favorite
@@ -16,6 +28,7 @@ class FavoriteSerializer(serializers.ModelSerializer):
 
 class PomodoroSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    html_link = LinkField()
 
     def create(self, validated_data):
         if "start" not in validated_data:
