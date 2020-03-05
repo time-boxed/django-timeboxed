fa418ee32fad641a26f6d99976beed8f4f655ac6
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-03-05 20:21:38 +0900

    Update some path names

--- pomodoro/urls.py
+++ pomodoro/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, re_path
 
 app_name = 'pomodoro'
 urlpatterns = [
-    re_path(r'^history/?(?P<date>\w+)?$', views.PomodoroHistory.as_view(), name='history'),
+    re_path(r'^history/?(?P<date>\w+)?$', views.PomodoroHistory.as_view(), name='pomodoro-list'),
     path('pomodoro/<int:pk>', views.PomodoroDetailView.as_view(), name='pomodoro-detail'),
     path('favorite/<int:pk>', views.Favorite.as_view(), name='favorite'),
     path('favorite', views.FavoriteList.as_view(), name='favorite-list'),
