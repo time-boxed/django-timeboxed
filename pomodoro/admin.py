e292b6249a59da2ff7c15af3d84802c8d374c14f
Paul Traylor <kungfudiscomonkey@gmail.com>
2023-11-29 19:18:25 +0900

    Update for deprecations

--- pomodoro/admin.py
+++ pomodoro/admin.py
@@ -1,7 +1,7 @@
 from . import models
 
 from django.contrib import admin
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 @admin.register(models.Project)
--- pomodoro/templatetags/pomodoro.py
+++ pomodoro/templatetags/pomodoro.py
@@ -8,7 +8,7 @@ from django import template
 from django.urls import reverse
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 
 register = template.Library()
 
